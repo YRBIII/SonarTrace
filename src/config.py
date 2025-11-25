315767f803b761c1708211929758772ee1dc0f15
Yung Brinney <Yungbrinney3@gmail.com>
2025-11-24 19:19:59 -0500

    added more py files

--- src/config.py
+++
@@ -1,9 +0,0 @@
-"""
-config.py - configuration constants
-"""
-from __future__ import annotations
-from pathlib import Path
-
-DEFAULT_REPORT_PREFIX = "host_enumeration_report"
-DEFAULT_OUTPUT_DIR = Path.cwd()
-REPORT_FILE_EXTENSION = ".txt"
